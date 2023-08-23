d97bf3d2bf9ea3eb79f9d9d0e288ad0b63391482
Kendall Screen <83034562+ktscreen@users.noreply.github.com>
2023-08-23 14:34:19 -0400

    Add files via upload

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -73,6 +73,7 @@ function updateDish(req, res) {
 	const { data: { name, description, price, image_url } = {} } = req.body;
 
 	res.locals.dish = {
+		id: res.locals.dishId,
 		name: name,
 		description: description,
 		price: price,
